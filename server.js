3665bf6666eb61870dcaffd518597751109201a1
Edward Gyampo <egyampo0101@gmail.com>
2021-06-11 11:55:29 +0000

    source update

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ let favicon = require('serve-favicon');
 let app = express();
 
 app.use(express.json());
-app.use(favicon(path.join(__dirname, 'src', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use('/', express.static('public'));
 
 app.get('/**/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
