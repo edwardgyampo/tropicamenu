181f4359a912683beadb82cf6da5e8f5b77bb8a5
Edward Gyampo <egyampo0101@gmail.com>
2026-08-15 12:43:54 +0000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { Menu } from './components/Menu';
-import { Stack } from './components/Stack';
 import { NavigationProvider } from './lib/navigation/Provider';
 
 function App() {
 
   return (
     <>
-      <Stack direction="row" style={{
-        alignItems: "center",
-        gap: "var(--space-4)"
-      }}>
-        <NavigationProvider>
-          <Menu />
-        </NavigationProvider>
-      </Stack>
+      <NavigationProvider>
+        <Menu />
+      </NavigationProvider>
     </>
   )
 }
